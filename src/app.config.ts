8fb581e19b3192a356d76e408fee82d8735a33f1
Arrrrray <junchaocode@gmail.com>
2020-08-27 15:11:13 +0800

    test

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,11 @@
 export default {
   // 页面路径列表
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/login/login',
+    'pages/apply/apply',
+    'pages/myApplyList/myApplyList',
+    'pages/applyStatistics/applyStatistics',
   ],
   // 全局的默认窗口表现
   window: {
